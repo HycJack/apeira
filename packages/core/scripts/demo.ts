730b97875144e010f7d1b8d0ae8fc8537a4766f4
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-09-23 19:31:03 +0800

    chore(demo): persist messages

--- packages/core/scripts/demo.ts
+++ packages/core/scripts/demo.ts
@@ -1,9 +1,14 @@
+import type { Message } from '@xsai/shared-chat'
+
+import fs from 'node:fs/promises'
 import { stdin as input, stdout as output } from 'node:process'
 import { createInterface } from 'node:readline/promises'
 
 import { ChatAgent } from '../src/agents/chat-agent'
 import { autoSaveMessages } from '../src/plugins/auto-save-messages'
 import { trimMessages } from '../src/plugins/trim-messages'
+
+const savedMessages = new URL('demo.messages.json', import.meta.url)
 
 const agent = new ChatAgent({
   instruction: 'You\'re a helpful assistant.',
@@ -13,7 +18,27 @@ const agent = new ChatAgent({
   },
   name: 'chat-agent',
   plugins: [
-    autoSaveMessages(),
+    autoSaveMessages({
+      load: async () => {
+        try {
+          const json = await fs.readFile(savedMessages, { encoding: 'utf8' })
+          // eslint-disable-next-line @masknet/type-prefer-return-type-annotation
+          return JSON.parse(json) as Message[]
+        }
+        catch {
+          console.error('auto-save-messages: load failed')
+          return []
+        }
+      },
+      save: async (messages) => {
+        try {
+          await fs.writeFile(savedMessages, JSON.stringify(messages))
+        }
+        catch {
+          console.error('auto-save-messages: save failed')
+        }
+      },
+    }),
     trimMessages(),
   ],
 })
