84acaf0af8d90e5c08689c57b0c021830ca57c37
藍+85CD <50108258+kwaa@users.noreply.github.com>
2026-05-13 22:17:42 +0800

    chore(docs): set base

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vitepress'
 
 export default defineConfig({
+  base: '/apeira/',
   description: 'A stream-first agent runtime for TypeScript.',
   themeConfig: {
     nav: [
